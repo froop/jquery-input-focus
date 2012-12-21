5a708fd7b8fdd8011e23a9a0b31f41542d6e105a
froop <frooprogrammer@gmail.com>
2012-12-21 21:15:54 +0900

    rename $next <- next

--- jquery.inputfocus.js
+++ jquery.inputfocus.js
@@ -63,7 +63,7 @@
 			};
 			var	k	=	e.keyCode;
 			var	s	=	e.shiftKey;
-			var	next	=	null;
+			var	$next	=	null;
 			var blKey	=	true;
 			if (!setting.enter&&k==13) return true;
 			if (!setting.tab&&k==9) return true;
@@ -90,7 +90,7 @@
 					//keyイベントを処理するもののみ抽出
 					if (!blKey){
 						//次のフォームオブジェクト探す
-						next = Focus_Move(s);
+						$next = Focus_Move(s);
 					}
 				break;
 				case 9:		//tab
@@ -100,7 +100,7 @@
 						break;
 					default:
 						//次のフォームオブジェクト探す
-						next = Focus_Move(s);
+						$next = Focus_Move(s);
 						blKey = false;
 						break;
 					}
@@ -114,7 +114,7 @@
 				if($.browser.msie) {
 					window.event.keyCode = 0;
 				}
-				focus(next);
+				focus($next);
 			}
 			return blKey;
 		};
